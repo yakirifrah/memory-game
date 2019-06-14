eef5e9d3ed05b109e0cbd400c1bab1fb0594236f
yakir <yakirifrah19@gmail.com>
2019-06-14 22:43:53 +0300

    finish masive logic game, workgit add .

--- js/game.js
+++ js/game.js
@@ -7,35 +7,56 @@ class Game {
         this.bord = new Board(COLUMN);
         this.elPreviousCard = null;
         this.flippedCouplesCount = 0;
+        this.isProcessing = false;
+
     }
     cardClicked(CurrentCard) {
         if (CurrentCard.classList.contains('flipped')) {
             return;
         }
-        CurrentCard.classList.add('flipped');
+        if (!this.isProcessing) {
+            CurrentCard.classList.add('flipped');
+        }
         this.checkIfCardFirst(CurrentCard);
     }
     checkIfCardFirst(CurrentCard) {
         if (this.elPreviousCard === null) {
-            console.log('first card');
             this.elPreviousCard = CurrentCard;
         }
         else {
-            console.log('second card');
             let Firstcard = this.elPreviousCard.getAttribute('data-img');
-            let Secondcard = CurrentCard.getAttribute('data-img');
-            this.isMatchCards(Firstcard, Secondcard);
-            this.elPreviousCard = null;
+            this.isMatchCards(Firstcard, CurrentCard);
+
         }
 
     }
-    isMatchCards(Firstcard, Secondcard) {
+    isMatchCards(Firstcard, CurrentCard) {
+        let Secondcard = CurrentCard.getAttribute('data-img');
         if (Firstcard === Secondcard) {
+            this.flippedCouplesCount++;
+            this.isVicotry(this.flippedCouplesCount, TOTAL_COUPLES_CARDS);
+            this.elPreviousCard = null;
             console.log('Right');
         }
         else {
             console.log('Wrong');
+            this.isProcessing = true;
+            setTimeout(() => {
+                if (this.elPreviousCard) {
+                    this.elPreviousCard.classList.remove('flipped');
+                }
+                CurrentCard.classList.remove('flipped');
+                this.elPreviousCard = null;
+                this.isProcessing = false;
+            }, 1000);
         }
+
+    }
+    isVicotry(flippedCouplesCount, TOTAL_COUPLES_CARDS) {
+        if (flippedCouplesCount === TOTAL_COUPLES_CARDS) {
+            console.log('victory');
+        }
+
 
     }
 
