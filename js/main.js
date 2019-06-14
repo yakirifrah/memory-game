16f34bad39291bd8c3b9f9e2bad59a5213c2237a
yakir <yakirifrah19@gmail.com>
2019-06-14 15:41:42 +0300

    adding function to init dom dynimiclly, workgit status

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
+
 let game = new Game();
 
 //cretae a board DOM
-$(document).ready(() => {
+
+$(document).ready(initDynmaicApp);
+
+function initDynmaicApp() {
     let conatainer = $('.container');
-
-})
-
-
-
+    for (let i = 0; i < ROW; i++) {
+        let row = $(`<div class ="row"></div>`)
+        for (let j = 0; j < COLUMN; j++) {
+            let col = $(`<div class="col-3 card-border"></div>`)
+            $(row).append(col);
+            $(conatainer).append(row);
+        }
+    }
+}
 
 
